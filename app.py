bdfd1b74df3e8e5f458ff67414c826b28ee741c2
Sufyan Ahmed <107124365+sufyanahmed1310@users.noreply.github.com>
2025-07-31 21:52:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+todos = []
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    todo = request.json.get('task')
+    todos.append(todo)
+    return jsonify({'task': todo}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
